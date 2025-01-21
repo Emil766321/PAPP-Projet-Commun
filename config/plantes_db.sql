74a91c35e797c41f55eea18a88c34b775500b230
Mustafa Kamal <muskamal@etmlnet.local>
2025-01-21 10:25:23 +0100

    Base de données des plantes et d'utilisateur.

--- config/plantes_db.sql
+++ config/plantes_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 21 jan. 2025 à 09:27
+-- Généré le : mar. 21 jan. 2025 à 10:18
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -22,7 +22,6 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
 CREATE DATABASE `plantes_db`;
 
 --
@@ -46,8 +45,27 @@ CREATE TABLE `plante` (
 --
 
 INSERT INTO `plante` (`id`, `Nom`, `Type_plante`, `Couleur`, `Besoin_lumiere`, `Arrosage`, `Temperateur_min`, `Temperateur_max`, `Description`) VALUES
-(1, 'Cactus', 'plante grasse', 'Verte', 'plein soleil', 'peu d\'arrosage', '5', '35', ''),
-(3, 'Cactus2', '', '', '', '', '', '', '');
+(1, 'Cactus', 'plante grasse', 'Verte', 'plein soleil', 'peu d\'arrosage', '5°C.', '35°C.', 'Le cactus est une plante succulente de la famille des Cactacées, souvent caractérisée par des tiges épaisses et épineuses qui lui permettent de stocker de l\'eau. Il préfère les environnements chauds et ensoleillés, avec un sol bien drainé et peu d\'arrosage. Certaines espèces produisent de belles fleurs colorées, généralement au printemps ou en été. Résistant à la sécheresse, le cactus est idéal pour les climats arides.'),
+(2, 'Persil', 'Herbacée, aromatique.', 'Vert', 'Lumière indirecte, partielleme', 'Sol modérément humide, arrosag', ' -10°C.', '21°C.', 'le persil est une plante assez adaptable, mais il préfère des températures modérées, une exposition à la lumière indirecte, et un sol légèrement humide. Lorsqu\'il est bien entretenu, il peut être une plante aromatique très productive.'),
+(3, 'Tomate', ' Herbacée, annuelle.', 'Fruits rouges (ou jaunes, oran', 'Plein soleil', 'Sol modérément humide, arrosag', '10°C.', '25°C.', 'La tomate préfère une exposition au plein soleil, un sol humide mais bien drainé, et des températures chaudes pour bien se développer.');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `Nom d'Utilisateur` varchar(10) NOT NULL,
+  `Mot de passe` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`Nom d'Utilisateur`, `Mot de passe`) VALUES
+('COFOP', 'cofopplante');
 
 --
 -- Index pour les tables déchargées
@@ -67,7 +85,7 @@ ALTER TABLE `plante`
 -- AUTO_INCREMENT pour la table `plante`
 --
 ALTER TABLE `plante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
