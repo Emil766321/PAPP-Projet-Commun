e236275124fd989013ffd49c054710a37dba7085
MinhQuang Ngo <minngo@etmlnet.local>
2025-01-28 15:47:59 +0100

    Déconnexion

--- src/html_php/index.php
+++ src/html_php/index.php
@@ -11,27 +11,27 @@
 
 
 <body>
+    <form action="Login.php" method="POST">
         <div name="tout"id ="tout">
             <div name="base" class="base">
                 <div name ="title_fond"class="okaybien">
-                    <h1 name="title"class = "Titre"> Profil de l'utilisateur</h1>   
-                    <img name="fond"id ="img" src ="https://png.pngtree.com/png-vector/20240327/ourlarge/pngtree-the-elderly-chef-s-legacy-isolated-on-a-transparent-background-png-image_12243109.png">
+                    <h1 name="username"class = "Titre">Profil de l'utilisateur</h1>   
+                    <img name="profileimage"id ="img" src ="https://png.pngtree.com/png-vector/20240327/ourlarge/pngtree-the-elderly-chef-s-legacy-isolated-on-a-transparent-background-png-image_12243109.png">
                 </div>
             </div>
 
             <div class="cofop" name="div_name">
                 <div id="Déconnexione" name="name"><h1 id ="cofop">COFOP</h1></div>
             </div>
-                <button class="Déconnexion" name="Déconnexion">Déconnexion</button>
-        </div>    
+            <a href="siteProjet-Commun.html" class="Déconnexion">Déconnexion</a>
+        </div>   
+        
+       
+       
+    </form>
 </body>
 
 
 
-</html
+</html>
 
-<?php
-session_start();
-session_destroy();
-header("location:connect.html")
-?>
