ac2231b5920d6bc8a7e8f435f808cdf0b6fb903e
Hugo Schweizer <hugschweize@etmlnet.local>
2025-02-04 09:35:42 +0100

    bouton déconexion

--- src/html_php/menu_site_projet.php
+++ src/html_php/menu_site_projet.php
@@ -5,64 +5,67 @@
         <link name="css" rel="stylesheet" href="..\ressources\css\menu_site_projet.css"/>
     </head>
     <body>
-    <a href="..\ressources\images\plus.png">
-    
-    <img id ="plus"src="..\ressources\images\plus.png">
-    </a>
-        <!-- petitVert = petit carré en haut à droite et titre = "Répertoire de plantes" -->
-        <div name="petitVert_titre" id="title">
-            <h1 name="titre" id="titre">R&#233;pertoire de plantes</h1>
-            <div name="petit_vert" id="mini"></div>
-        </div>
+        <a href="..\ressources\images\plus.png">
+        <img id ="plus"src="..\ressources\images\plus.png">
+        </a>
 
-        <?php
-        
-        //tableau php
-        $elements = [
-            [
-                'name' => 'Plante1', 
-                'image' => '../ressources/images/brocoli.png'
-            ],
-            [
-                'name' => 'Plante2', 
-                'image' => '../ressources/images/brocoli.png'
-            ],
-            [
-                'name' => 'Plante3', 
-                'image' => '../ressources/images/brocoli.png'
-            ],
-        ];
-        ?>
-        <!-- div grandVert = fond vert -->
-        <div name="grandVert" class="vert">
+            <!-- petitVert = petit carré en haut à droite et titre = "Répertoire de plantes" -->
+            <div id="heade">
+                <div id="Deconnexion"><h1 id ="decoTxt">D&#233;connexion</h1></div>
+                <div name="petitVert_titre" id="title">
+                    <h1 name="titre" id="titre">R&#233;pertoire de plantes</h1>
+                    <div name="petit_vert" id="mini"></div>
+                </div>
+            </div>
 
             <?php
-            foreach ($elements as $item) {
-                ?>
-                    <!-- une dive = une carte de plante -->
-                    <div class="card">
-                        <a class="card" href="..\ressources\images\brocoli.png">
-                            <div class="title-card">
-                                <p><?php echo $item['name'];?></p>
-                            </div>
-                            <div class="img-card">
-                                <img class="img" src="<?php echo $item['image'];?>" alt="image plante">
-                            </div>
-                            <div class="crayon">
-                                <a href="..\ressources\images\crayon.png">
-                                    <img class="img-crayon" src="../ressources/images/crayon.png" alt="image plante">
-                                </a>
-                            </div>
-                            <div class="croix">
-                                <a href="..\ressources\images\croix.png">
-                                    <img class="img-croix" src="../ressources/images/croix.png" alt="image plante">
-                                </a>
-                            </div>
-                        </a>
-                    </div>
-                <?php
-            }
+            
+            //tableau php
+            $elements = [
+                [
+                    'name' => 'Plante1', 
+                    'image' => '../ressources/images/brocoli.png'
+                ],
+                [
+                    'name' => 'Plante2', 
+                    'image' => '../ressources/images/brocoli.png'
+                ],
+                [
+                    'name' => 'Plante3', 
+                    'image' => '../ressources/images/brocoli.png'
+                ],
+            ];
             ?>
-        </div>
+            <!-- div grandVert = fond vert -->
+            <div name="grandVert" class="vert">
+
+                <?php
+                foreach ($elements as $item) {
+                    ?>
+                        <!-- une dive = une carte de plante -->
+                        <div class="card">
+                            <a id="br"class="card" href="..\ressources\images\brocoli.png">
+                                <div class="title-card">
+                                    <p><?php echo $item['name'];?></p>
+                                </div>
+                                <div class="img-card">
+                                    <img class="img" src="<?php echo $item['image'];?>" alt="image plante">
+                                </div>
+                                <div class="crayon">
+                                    <a href="..\ressources\images\crayon.png">
+                                        <img class="img-crayon" src="../ressources/images/crayon.png" alt="image plante">
+                                    </a>
+                                </div>
+                                <div class="croix">
+                                    <a href="..\ressources\images\croix.png">
+                                        <img class="img-croix" src="../ressources/images/croix.png" alt="image plante">
+                                    </a>
+                                </div>
+                            </a>
+                        </div>
+                    <?php
+                }
+                ?>
+            </div>
     </body>
 </html>
