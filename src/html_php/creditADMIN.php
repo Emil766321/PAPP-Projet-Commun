33a0891d3f704dcfbc4c7c30072ce95624dece3f
Mustafa Kamal <muskamal@etmlnet.local>
2025-02-13 09:02:09 +0100

    solving the problems

--- src/html_php/creditADMIN.php
+++ src/html_php/creditADMIN.php
@@ -1,16 +1,9 @@
-<?php
-session_start();
-if(!isset($_SESSION["user"])){
-    header("location:index.php");
-    exit();
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../ressources/css/styleCredit.css">
+    <link rel="stylesheet" href="../ressources/css/credits_style.css">
     <title>credits</title>
 </head>
 <body>
